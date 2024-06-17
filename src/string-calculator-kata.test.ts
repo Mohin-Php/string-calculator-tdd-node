205ec08e4298d5fe20d737ec8a82137fcc69d942
MohinSandhi <88226058+MohinSandhi@users.noreply.github.com>
2024-06-17 18:43:36 +0530

    test(string-calculator): disallow negative numbers

--- src/string-calculator-kata.test.ts
+++ src/string-calculator-kata.test.ts
@@ -1,5 +1,6 @@
 import StringCalculatorKata from './string-calculator-kata'
 import 'jest';
+import {NegativeNumberExpectation} from './negetive-number-expecation';
 
 
 describe("String Calculator Kata", () => {
@@ -7,5 +8,10 @@ describe("String Calculator Kata", () => {
 
     test('return 0 for an empty string', () => {
         expect(stringCalculatorKata.add('')).toBe(0);
+    });
+
+    test('disallow negative numbers', () => {
+        expect(() => stringCalculatorKata.add('1,-2,3')).toThrow(NegativeNumberExpectation);
+        expect(() => stringCalculatorKata.add('1,-2,3')).toThrow('negative numbers not allowed -2');
     });
 })
