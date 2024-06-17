19c6ee1138f3ca323e8f37c1d9da4ef82366c535
MohinSandhi <88226058+MohinSandhi@users.noreply.github.com>
2024-06-17 18:44:20 +0530

    test(string-calculator): sum the numbers from the string

--- src/string-calculator-kata.test.ts
+++ src/string-calculator-kata.test.ts
@@ -14,4 +14,14 @@ describe("String Calculator Kata", () => {
         expect(() => stringCalculatorKata.add('1,-2,3')).toThrow(NegativeNumberExpectation);
         expect(() => stringCalculatorKata.add('1,-2,3')).toThrow('negative numbers not allowed -2');
     });
+
+    test('return the number if a single number is received', () => {
+        expect(stringCalculatorKata.add('1')).toBe(1);
+        expect(stringCalculatorKata.add('5')).toBe(5);
+    });
+
+    test('sum the numbers from the string', () => {
+        expect(stringCalculatorKata.add('1,2')).toBe(3);
+        expect(stringCalculatorKata.add('5,5')).toBe(10);
+    })
 })
